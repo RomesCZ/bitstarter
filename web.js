26c89bd2cfeb8dcbca84a664062aae1b29f60c9b
Roman Silar <silar@centrum.cz>
2013-07-10 19:46:09 +0000

    Plus index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  /response.send('Hello World 2!');
+  response.send
+  
 });
 
 var port = process.env.PORT || 5000;
